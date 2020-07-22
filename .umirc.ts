df6bb8de2225faec971d0817f9e1cad5382e284b
陈浩银 <chen.haoyin@angsivip.com>
2020-07-22 13:55:58 +0800

    feat：menu整理

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from 'dumi';
 
+import menus from './src/menu';
+
 export default defineConfig({
-  title: 'AngsiDI',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  // mode: 'site',
+  title: 'Angsi UUI',
+  // favicon:
+  //   'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  // logo:
+  //   'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  menus,
+  //  {
+  //   '/blog': [
+  //     {
+  //       title: '简介',
+  //     },
+  //     {
+  //       title: '辟邪剑谱',
+  //       children: ['blog/test'],
+  //     },
+  //     {
+  //       title: '降龙十八掌',
+  //       children: ['blog/test2'],
+  //     },
+  //   ],
+  // },
   // more config: https://d.umijs.org/config
 });
---
+++ src/menu.ts
@@ -0,0 +1,15 @@
+export default {
+  '/blog': [
+    {
+      title: '简介',
+    },
+    {
+      title: '辟邪剑谱',
+      children: ['blog/test'],
+    },
+    {
+      title: '降龙十八掌',
+      children: ['blog/test2'],
+    },
+  ],
+};
