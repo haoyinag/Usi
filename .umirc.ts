dceab27832bcc4894f51ace77f1bcfff9a5a1a86
陈浩银 <chen.haoyin@angsivip.com>
2020-07-23 17:36:09 +0800

    feat：组件封装区分依赖

--- .umirc.ts
+++ .umirc.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   //   'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
 
-  menus,
+  // menus,
   navs: [
     // null, // null 值代表保留约定式生成的导航，只做增量配置
     {
