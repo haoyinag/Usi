c2fd290cce3c5746b7c70305e99de4975a52889e
陈浩银 <chen.haoyin@angsivip.com>
2020-07-23 16:20:35 +0800

    feat：修改title

--- .umirc.ts
+++ .umirc.ts
@@ -5,7 +5,7 @@ import menus from './src/menu';
 
 export default defineConfig({
   mode: 'site',
-  title: 'Angsi UUI',
+  title: 'UUI',
   // favicon:
   //   'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   // logo:
