a9362fffddebaac4654d167c1c8b579ffdc1a1fd
陈浩银 <chen.haoyin@angsivip.com>
2020-07-24 09:21:49 +0800

    Updates

---
+++ menu.d.ts
@@ -0,0 +1,15 @@
+declare const _default: {
+    '/Utils': {
+        title: string;
+        children: string[];
+    }[];
+    '/Hooks': {
+        title: string;
+        children: string[];
+    }[];
+    '/Services': {
+        title: string;
+        children: string[];
+    }[];
+};
+export default _default;
