6ad15f66dc07f4d809d645b6e89741fa5d00493e
陈浩银 <chen.haoyin@angsivip.com>
2020-07-30 09:58:47 +0800

    feat：打包配置

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,45 @@ import { defineConfig } from 'dumi';
 // import menus from './src/menu';
 // import routes from './src/router';
 
-export default defineConfig({
+const env = process.env.NODE_ENV;
+
+const chainWebpackPro = (config: any) => {
+  /** 打包优化 */
+  config.merge({
+    optimization: {
+      minimize: true,
+      splitChunks: {
+        chunks: 'all',
+        minSize: 30000,
+        minChunks: 3,
+        automaticNameDelimiter: '.',
+        cacheGroups: {
+          vendor: {
+            name: 'vendors',
+            test(testConfit: any) {
+              return /[\\/]node_modules[\\/]/.test(testConfit.resource);
+            },
+            priority: 10,
+          },
+          icons: {
+            name: 'icons',
+            chunks: 'all',
+            test: /[\\/]node_modules[\\/](@ant-design)[\\/]/,
+          },
+          commons: {
+            name: 'commons',
+            chunks: 'async',
+            minChunks: 2,
+            minSize: 0,
+          },
+        },
+      },
+    },
+  });
+  return config;
+};
+
+let config: any = {
   base: '/',
   publicPath: '/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
@@ -14,16 +52,6 @@ export default defineConfig({
   // logo:
   //   'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'dist',
-
-  // menus,
-  // hero: {
-  //   title: 'Hooks',
-  //   desc: '12321313',
-  //   actions: {
-  //     text: 'Getting Started',
-  //     link: '/index',
-  //   },
-  // },
   navs: [
     // null, // null 值代表保留约定式生成的导航，只做增量配置
     {
@@ -45,11 +73,6 @@ export default defineConfig({
     {
       title: '小程序',
       path: '/miniapp',
-      // path: '/miniapp/mini',
-      // children: [
-      //   { title: '原生小程序', path: '/miniapp/mini' },
-      //   { title: 'Taro 多端', path: '/miniapp/taro' },
-      // ],
     },
     {
       title: 'GitHub',
@@ -61,4 +84,14 @@ export default defineConfig({
     },
   ],
   // more config: https://d.umijs.org/config
-});
+};
+
+if (env === 'production') {
+  config = {
+    ...config,
+    // chunks: ['vendors', 'umi'],
+    chainWebpack: (config: any) => chainWebpackPro(config),
+  };
+}
+
+export default defineConfig(config);
